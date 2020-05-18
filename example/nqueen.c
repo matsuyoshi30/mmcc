5269f6486bd0efaea197c62f80347171f5d62e83
matsuyoshi <sfbgwm30@gmail.com>
2020-05-18 23:04:20 +0900

    Add example

---
+++ example/nqueen.c
@@ -0,0 +1,56 @@
+// How to run
+
+// $ make
+// $ ./mmcc example/nqueen.c > tmp.s
+// $ cc -static -o tmp tmp.s
+// $ ./tmp
+
+int print_board(int (*board)[8]) {
+  for (int i=0; i<8; i=i+1) {
+    for (int j=0; j<8; j=j+1)
+      if (board[i][j])
+          printf("Q ");
+      else
+          printf(". ");
+    printf("\n");
+  }
+  printf("\n\n");
+}
+
+int conflict(int (*board)[8], int row, int col) {
+  for (int i=0; i<row; i=i+1) {
+    if (board[i][col])
+      return 1;
+    int j = row - i;
+    if (0 < col-j+1)
+      if (board[i][col-j])
+        return 1;
+    if (col+j < 10)
+      if (board[i][col+j])
+        return 1;
+  }
+  return 0;
+}
+
+int solve(int (*board)[8], int row) {
+  if (row == 8) {
+    print_board(board);
+    return 0;
+  }
+  for (int i=0; i<8; i=i+1) {
+    if (conflict(board, row, i)) {
+    } else {
+      board[row][i] = 1;
+      solve(board, row+1);
+      board[row][i] = 0;
+    }
+  }
+}
+
+int main() {
+  int board[64];
+  for (int i=0; i<64; i=i+1)
+    board[i] = 0;
+  solve(board, 0);
+  return 0;
+}
