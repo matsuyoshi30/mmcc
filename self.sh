04e95294a2206a31c7464d865a1b815be5e2d53b
matsuyoshi <sfbgwm30@gmail.com>
2020-09-11 16:15:12 +0900

    Add codegen.c to frankenbuild

--- self.sh
+++ self.sh
@@ -7,26 +7,10 @@ mkdir -p $TMP
 
 mmcc() {
 	  file=$1
-	  cat <<EOF > $TMP/$1
-typedef struct FILE FILE;
-extern FILE *stdout;
-extern FILE *stderr;
-
-void *malloc(long size);
-void *calloc(long nmemb, long size);
-int *__errno_location();
-char *strerror(int errnum);
-FILE *fopen(char *pathname, char *mode);
-long fread(void *ptr, long size, long nmemb, FILE *stream);
-int feof(FILE *stream);
-static void assert() {}
-int strcmp(char *s1, char *s2);
-EOF
 
 	  grep -v '^#' mmcc.h >> $TMP/$1
 	  grep -v '^#' $1 >> $TMP/$1
     sed -i 's/\bbool\b/_Bool/g' $TMP/$1
-    sed -i 's/\berrno\b/*__errno_location()/g' $TMP/$1
     sed -i 's/, \.\.\.//g' $TMP/$1
 
 	  ./mmcc $TMP/$1 > $TMP/${1%.c}.s
@@ -40,6 +24,6 @@ cc() {
 mmcc main.c
 cc tokenize.c
 cc parse.c
-cc codegen.c
+mmcc codegen.c
 
 gcc -static -o mmcc-gen2 $TMP/*.o
