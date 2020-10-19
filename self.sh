f9b4d3f747ce1be6584772f38a81e5d07cc34261
matsuyoshi <sfbgwm30@gmail.com>
2020-10-20 00:14:42 +0900

    Self-host: except the C preprocessor, mmcc can now build itself

--- self.sh
+++ self.sh
@@ -3,11 +3,29 @@ set -e
 
 TMP=tmp-self
 
+rm -rf $TMP
 mkdir -p $TMP
 
 mmcc() {
 	  file=$1
     cat <<EOF > $TMP/$1
+typedef struct FILE FILE;
+extern FILE *stdin;
+extern FILE *stdout;
+extern FILE *stderr;
+
+int *__errno_location();
+
+typedef struct {
+  int gp_offset;
+  int fp_offset;
+  void *overflow_arg_area;
+  void *reg_save_area;
+} __va_elem;
+typedef __va_elem va_list[1];
+int vfprintf(FILE *stderr, char *fmt, va_list ap);
+int vprintf(char *fmt, va_list ap);
+static void va_end(__va_elem *ap) {}
 EOF
 
 	  grep -v '^#' mmcc.h >> $TMP/$1
@@ -15,10 +33,11 @@ EOF
     sed -i 's/\bbool\b/_Bool/g' $TMP/$1
     sed -i 's/\btrue\b/1/g; s/\bfalse\b/0/g;' $TMP/$1
     sed -i 's/\bNULL\b/0/g' $TMP/$1
-    sed -i 's/, \.\.\.//g' $TMP/$1
+    sed -i 's/\berrno\b/*__errno_location()/g' $TMP/$1
+    sed -i 's/\bva_start\b/__builtin_va_start/g' $TMP/$1
 
 	  ./mmcc $TMP/$1 > $TMP/${1%.c}.s
-    gcc -c -o $TMP/${1%.c}.o $TMP/${1%.c}.s
+    gcc -g -c -o $TMP/${1%.c}.o $TMP/${1%.c}.s
 }
 
 cc() {
@@ -26,9 +45,9 @@ cc() {
 }
 
 mmcc main.c
-cc tokenize.c
+mmcc tokenize.c
 mmcc type.c
 mmcc parse.c
 mmcc codegen.c
 
-gcc -static -o mmcc-gen2 $TMP/*.o
+gcc -g -static -o mmcc-gen2 $TMP/*.o
